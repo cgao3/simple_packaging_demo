41bf9cf51aace90f9dbe09ae61b72fec90aed4c3
Chao Gao <cgao3@ualberta.ca>
2023-06-09 16:48:54 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = "hello test",
+    version = "v0.1",
+    url = "http://xx.yy.zz",
+    author = "Chao Gao",
+    author_email = "xx@xx.com",
+    package=["this_is_test"]
+)
